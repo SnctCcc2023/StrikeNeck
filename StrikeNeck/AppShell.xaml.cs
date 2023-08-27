363d713767b780cae36fc982f28a8365097eaadd
4geniu <taketubo0714@gmail.com>
2023-08-28 02:47:33 +0900

    init

---
+++ StrikeNeck/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace strikeneck
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ StrikeNeck/StrikeNeck/AppSetting/NotificationInterval.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrikeNeck.StrikeNeck.AppSetting
+{
+    public class NotificationInterval
+    {
+        private readonly NotificationIntervalList notificationInterval;
+
+        public NotificationInterval(NotificationIntervalList notificationInterval = NotificationIntervalList.fifteenMinutes)
+        {
+            this.notificationInterval = notificationInterval;
+        }
+    }
+
+    public enum NotificationIntervalList
+    {
+        oneMinute = 1,
+        fifteenMinutes = 15,
+        thirtyMinutes = 30,
+        oneHour = 60
+    };
+}
