06962ff258fc32e7c2cb1880363de385aaa54a0b
haru210 <harun20070320@gmail.com>
2023-09-03 00:21:19 +0900

    make a feature that user can take a photo

--- StrikeNeck/Init/Init1.xaml.cs
+++ StrikeNeck/Init/Init1.xaml.cs
@@ -1,7 +1,10 @@
+using System.Data;
+
 namespace strikeneck.Init
 {
     public partial class Init1 : ContentPage
     {
+        private TimeOnly StartTime = TimeOnly.FromDateTime(DateTime.Now);
         public Init1()
         {
             InitializeComponent();
@@ -9,6 +12,30 @@ namespace strikeneck.Init
         private async void OnClicked(object sender, EventArgs e)
         {
             await Shell.Current.GoToAsync("//Init2");
+        }
+        private void cameraView_CamerasLoaded(object sender, EventArgs e)
+        {
+            cameraView.Camera = cameraView.Cameras.First();
+            MainThread.BeginInvokeOnMainThread(async () =>
+            {
+                await cameraView.StopCameraAsync();
+                var result = await cameraView.StartCameraAsync();
+
+            });
+        }
+        private async void TakePhoto(object sender, EventArgs e)
+        {
+            while (true)
+            {
+                await Task.Delay(TimeSpan.FromMilliseconds(10));
+                StartTime = StartTime.Add(TimeSpan.FromMilliseconds(-10));
+
+                if (StartTime.Second %10 == 0)
+                {
+                    StartTime = TimeOnly.FromDateTime(DateTime.Now);
+                    myImage.Source = cameraView.GetSnapShot(Camera.MAUI.ImageFormat.PNG);
+                }
+            }
         }
     }
 }
