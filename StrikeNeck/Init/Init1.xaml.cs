09f6b266ba01fc0ba3f40d89fa7708c5f0638ec8
KeitaroHamada <hamataro1029@gmail.com>
2023-09-04 10:28:24 +0900

    設定のデザイン

--- StrikeNeck/Init/Init1.xaml.cs
+++ StrikeNeck/Init/Init1.xaml.cs
@@ -1,4 +1,9 @@
+using System;
+using Xamarin.Essentials;
+using System.Threading.Tasks;
+
 namespace strikeneck.Init
+
 {
     public partial class Init1 : ContentPage
     {
@@ -6,9 +11,12 @@ namespace strikeneck.Init
         {
             InitializeComponent();
         }
+
         private async void OnClicked(object sender, EventArgs e)
         {
             await Shell.Current.GoToAsync("//Init2");
         }
+
     }
+
 }
