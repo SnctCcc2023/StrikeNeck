92edbf102f2a481d077bceced62b0081300bd9a8
4geniu <taketubo0714@gmail.com>
2023-09-09 16:34:57 +0900

    feat : The interface of the forward leaning object is defined.

---
+++ StrikeNeck/AppSetting/Setting.cs
@@ -0,0 +1,23 @@
+﻿namespace StrikeNeck.AppSetting
+{
+    public class Setting
+    {
+        readonly bool isNotificationEnabled;
+        readonly NotificationInterval notificationInterval;
+        readonly DetectionSensitivity detectionSensitivity;
+
+        public Setting(NotificationInterval notificationInterval, DetectionSensitivity detectionSensitivity, bool isNotificationEnabled)
+        {
+            this.notificationInterval = notificationInterval;
+            this.isNotificationEnabled = isNotificationEnabled;
+            this.detectionSensitivity = detectionSensitivity;
+        }
+
+        public Setting()
+        {
+            detectionSensitivity = new DetectionSensitivity();
+            isNotificationEnabled = true;
+            notificationInterval = new NotificationInterval();
+        }
+    }
+}
---
+++ StrikeNeck/Imaging/ForwardLeanDetector.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace strikeneck.Imaging
+{
+    //前傾姿勢判定器を表すクラス
+    public class ForwardLeanDetector
+    {
+        private AttitudeEstimator estimator;
+        
+        public ForwardLeanDetector()
+        {
+            
+        }
+
+        //前傾姿勢かどうかを判定する
+        public bool IsForwardLeaned()
+        {
+            //本来は識別モデルで判定するが、現在は仮として常にtrueを返す
+            return true;
+        }
+
+    }
+}
