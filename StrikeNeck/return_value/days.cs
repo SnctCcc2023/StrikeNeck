6d1a9ad011f7d0ec1610e268af2dadfbd35669cf
r04i02 <k10190202@gmail.com>
2023-09-27 02:57:23 +0900

    見やすさの改善（一部）

--- StrikeNeck/return_value/days.cs
+++ StrikeNeck/return_value/days.cs
@@ -6,19 +6,19 @@ using System.Collections.Generic;
 
 
 public class Day_Returnee
-{ 
+{
     public Dictionary<List<int>, List<float>> DayResult = new(); //これがDBの情報を持っている
     public void DayReturnee() //この関数の呼び出しによってメンバ変数を更新
     {
-        
-            DateTime dt = DateTime.Now;
-            DayOfWeek today = dt.DayOfWeek;
 
-            var day_checker = new List<int>() { }; //下の方に説明あるよ
-            var Result_day = new List<float>() { }; //下の方に説明あるよ
-            var date = DateTime.Now;
+        DateTime dt = DateTime.Now;
+        DayOfWeek today = dt.DayOfWeek;
 
-            var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;
+        var day_checker = new List<int>() { }; //下の方に説明あるよ
+        var Result_day = new List<float>() { }; //下の方に説明あるよ
+        var date = DateTime.Now;
+
+        var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;
                                  Initial Catalog=days;
                                  Integrated Security=True;
                                  Connect Timeout=30;
@@ -26,88 +26,88 @@ public class Day_Returnee
                                  Trust Server Certificate=False;
                                  Application Intent=ReadWrite;
                                  Multi Subnet Failover=False";
-            // daysのDBの接続文字列
+        // daysのDBの接続文字列
 
-            switch (today)
-            {
-                case DayOfWeek.Sunday:
-                    date = date.AddDays(0);
-                    break;
-                case DayOfWeek.Monday:
-                    date = date.AddDays(-1);
-                    break;
-                case DayOfWeek.Tuesday:
-                    date = date.AddDays(-2);
-                    break;
-                case DayOfWeek.Wednesday:
-                    date = date.AddDays(-3);
-                    break;
-                case DayOfWeek.Thursday:
-                    date = date.AddDays(-4);
-                    break;
-                case DayOfWeek.Friday:
-                    date = date.AddDays(-5);
-                    break;
-                case DayOfWeek.Saturday:
-                    date = date.AddDays(-6);
-                    break;
-            }
+        switch (today)
+        {
+            case DayOfWeek.Sunday:
+                date = date.AddDays(0);
+                break;
+            case DayOfWeek.Monday:
+                date = date.AddDays(-1);
+                break;
+            case DayOfWeek.Tuesday:
+                date = date.AddDays(-2);
+                break;
+            case DayOfWeek.Wednesday:
+                date = date.AddDays(-3);
+                break;
+            case DayOfWeek.Thursday:
+                date = date.AddDays(-4);
+                break;
+            case DayOfWeek.Friday:
+                date = date.AddDays(-5);
+                break;
+            case DayOfWeek.Saturday:
+                date = date.AddDays(-6);
+                break;
+        }
 
-            SqlConnection connection = new(connectionString);
-            connection.Open();
-            for (int j = 0; j < 9; j++)
+        SqlConnection connection = new(connectionString);
+        connection.Open();
+        for (int j = 0; j < 9; j++)
+        {
+            for (int i = 0; i < 7; i++)
             {
-                for (int i = 0; i < 7; i++)
+                string sql = "SELECT day_detection_count,forward_lean_count FROM Table2 WHERE day = @date";
+                using SqlCommand command = new(sql, connection);
+
+                // パラメータの追加
+                command.Parameters.AddWithValue("@date", date);
+
+                using SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
                 {
-                    string sql = "SELECT day_detection_count,forward_lean_count FROM Table2 WHERE day = @date";
-                    using SqlCommand command = new(sql, connection);
-
-                    // パラメータの追加
-                    command.Parameters.AddWithValue("@date", date);
-
-                    using SqlDataReader reader = command.ExecuteReader();
-                    while (reader.Read())
+                    if (reader.Read())
                     {
-                        if (reader.Read())
+                        float float1 = reader.GetFloat(0);
+                        float float2 = reader.GetFloat(1);
+                        Result_day[0] = float1; //0が最新の週間
+                        Result_day[1] = float2; //0が日曜、1が月曜…
+                        day_checker[0] = j / 60; //アプリが何時間起動していたか
+                        day_checker[1] = i / 60; //前傾姿勢が何時間だったか 
+                        if (DayResult.ContainsKey(day_checker))
                         {
-                            float float1 = reader.GetFloat(0);
-                            float float2 = reader.GetFloat(1);
-                            Result_day[0] = float1; //0が最新の週間
-                            Result_day[1] = float2; //0が日曜、1が月曜…
-                            day_checker[0] = j / 60; //アプリが何時間起動していたか
-                            day_checker[1] = i / 60; //前傾姿勢が何時間だったか 
-                            if (DayResult.ContainsKey(day_checker))
-                            {
-                                DayResult[day_checker] = Result_day;
-                            }
-                            else
-                            {
-                                DayResult.Add(day_checker, Result_day);
-                            }
-
+                            DayResult[day_checker] = Result_day;
                         }
                         else
                         {
-                            Result_day[0] = 0; //0が最新の週間
-                            Result_day[1] = 0; //0が日曜、1が月曜…
-                            day_checker[0] = j; //アプリが何時間起動していたか
-                            day_checker[1] = i; //前傾姿勢が何時間だったか 
-                            if (DayResult.ContainsKey(day_checker))
-                            {
-                                DayResult[day_checker] = Result_day;
-                            }
-                            else
-                            {
-                                DayResult.Add(day_checker, Result_day);
-                            }
+                            DayResult.Add(day_checker, Result_day);
+                        }
+
+                    }
+                    else
+                    {
+                        Result_day[0] = 0; //0が最新の週間
+                        Result_day[1] = 0; //0が日曜、1が月曜…
+                        day_checker[0] = j; //アプリが何時間起動していたか
+                        day_checker[1] = i; //前傾姿勢が何時間だったか 
+                        if (DayResult.ContainsKey(day_checker))
+                        {
+                            DayResult[day_checker] = Result_day;
+                        }
+                        else
+                        {
+                            DayResult.Add(day_checker, Result_day);
                         }
                     }
-                    date = date.AddDays(1);
                 }
-                date = date.AddDays(-14);
+                date = date.AddDays(1);
             }
+            date = date.AddDays(-14);
         }
     }
+}
 
 
 
