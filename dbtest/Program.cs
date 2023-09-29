373423080cd9080e474cbd18eb935d7a7f9e7707
4geniu <taketubo0714@gmail.com>
2023-09-29 11:31:53 +0900

    gg

---
+++ dbtest/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.Sqlite;
+using Csv;
+namespace dbtest
+{
+    public class DbTest
+    {
+        public static void Main()
+        {
+            DBAccessor.SaveResult(true, new DateTime(2020, 3, 5, 10, 0, 0));
+            var res = DBAccessor.GetAnalyticsPerDay(new DateTime(2020, 3, 5, 10, 0, 0));
+            foreach(var r in res)
+            {
+                Console.WriteLine(r.Date);
+                Console.WriteLine(r.ActiveTime);
+                Console.WriteLine(r.FowardLeanTime);
+            }
+        }
+    }
+}
