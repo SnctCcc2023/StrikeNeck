19f0ca0e03ab55e9e874d81fae737f3bf5ff1a5f
r04i02 <k10190202@gmail.com>
2023-10-10 20:45:06 +0900

    restart

---
+++ StrikeNeck/DB Result/DataGetter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+namespace DataGetter;
+
+public class DataGetter
+{
+	public int HourDataGet(int SerectHour)
+	{
+
+	}
+
+    public int DailyDataGet(int SerectDay)
+	{
+
+	}
+
+    public int YearlyDataGet(int SerectMonth)
+	{
+
+	}
+}
---
+++ StrikeNeck/DB Result/DataSaver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+namespace DateSetter;
+
+public class DateSaver
+{
+    private bool FirstTimeChecker = true;
+    public void DataSave(bool result) //データのセーブの際に呼び出す。結果をbool型で渡す
+	{
+        if (FirstTimeChecker)
+        {
+            CreateTable(); //初回のみテーブルを作成する
+            FirstTimeChecker = false;
+        }
+    }
+
+    private void CreateTable() 
+    {
+
+    }
+}
