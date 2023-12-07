51dd55bf72f45592c631d04ce31c8ec45a491df5
r04i02 <k10190202@gmail.com>
2023-12-07 22:23:36 +0900

    make create table call ResultSave

---
+++ NewStrikeNeckDB/DataControl/DBCreator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+using System.Configuration;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text;
+using NewDB.DataControl;
+
+namespace StrikeNeckDB.DataControl
+{
+    public class DBCreator
+    {
+        SQLCommandExecuter tmp = new SQLCommandExecuter();
+        public void MinuteResultSaveDBCreator()
+        {
+            StringBuilder query = new StringBuilder();
+            query.Clear();
+            query.Append("CREATE TABLE IF NOT EXISTS HourlyData(");
+            query.Append("DATETIME TEXT NOT NULL");
+            query.Append(",RESULT INTEGER NOT NULL");
+            query.Append(",primary key(DATETIME)");
+            query.Append(");");
+
+            tmp.ExecuteNonQuery(query.ToString());
+        }
+
+        public void HourResultSaveDBCreator()
+        {
+            StringBuilder query = new StringBuilder();
+            query.Clear();
+            query.Append("CREATE TABLE IF NOT EXISTS DailyData(");
+            query.Append("DATETIME TEXT NOT NULL");
+            query.Append(",UptimeMinute INTEGER NOT NULL");
+            query.Append(",ForwardLeanMinute INTEGER NOT NULL");
+            query.Append(",primary key(DATETIME)");
+            query.Append(");");
+
+            tmp.ExecuteNonQuery(query.ToString());
+        }
+
+        public void DayResultSaveDBCreator()
+        {
+            StringBuilder query = new StringBuilder();
+            query.Clear();
+            query.Append("CREATE TABLE IF NOT EXISTS YearlyData(");
+            query.Append("DATETIME TEXT NOT NULL");
+            query.Append(",UptimeHour INTEGER NOT NULL");
+            query.Append(",ForwardLeanHour NOT NULL");
+            query.Append(",primary key(DATETIME)");
+            query.Append(");");
+        }
+    }
+}
---
+++ NewStrikeNeckDB/Program.cs
@@ -0,0 +1,21 @@
+﻿using StrikeNeckDB.DataControl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewStrikeNeckDB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DBCreator tmp = new DBCreator();
+            tmp.MinuteResultSaveDBCreator();
+            tmp.HourResultSaveDBCreator();
+            tmp.DayResultSaveDBCreator();
+            
+        }
+    }
+}
