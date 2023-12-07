2fd21cdd8f7b2d5a655fb37023846c99cf7eb7ea
r04i02 <k10190202@gmail.com>
2023-12-07 19:57:44 +0900

    init

--- NewDB/Test/main.cs
+++ NewDB/Test/main.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NewDB
+namespace StrikeNeckDB
 {
-    public class Class1
+    public class main
     {
+
     }
 }
